21cf9e7c67246c9e846ae31f97c71417bfeee54c
Dorlmo <86467190+Dorlmo@users.noreply.github.com>
2023-08-20 14:29:56 +0800

    added:feature update #I7SN2Z

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import routes from './config/routes'
 import { createPinia } from 'pinia'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
@@ -20,6 +21,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
+app.use(ElementPlus)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
